398f51a8acc913afcd982c10939ccbb77ae504ce
Denis <varenikovda@gmail.com>
2022-05-09 21:33:03 +0300

    Initial Commit

---
+++ WateringPlants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WateringPlants
+//
+//  Created by Денис on 09.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
